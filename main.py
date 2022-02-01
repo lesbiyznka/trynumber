90e8ad0edcc9dbfd3d6059585d6292fd23552643
lesbiyznka <lizavladykina6@gmail.com>
2022-02-01 18:56:53 +0500

    Проверка ввода пользователя

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import time
 x = int(time.time())
 
+# Проверка ввода пользователя
+def check_input(user_input):
+    if 1 <= int(user_input) <= 10:
+        return True
+    return False
 
+
+# LCG генератор чисел
 def random(a=6364136223846793005, c=1442695040888963407, m=2 ** 64):
     global x
     x = (a * x + c) % m
     return x
 
+# Основной игровой цикл
+while True:
+    print("Если угадай число от 1 до 10")
 
-for i in range(15):
-    print(random() % 10)
+    n = input()
+
+    if not check_input(n):
+        print("хуню ввел")
+        continue
+
+
+
+
+
